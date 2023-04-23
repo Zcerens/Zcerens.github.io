c65d02a349d364abb6f6af9edf716d6ce59b588d
Zcerens <matricariaxx@gmail.com>
2023-04-23 22:22:47 +0300

    Acitivty 6 is done

--- Activity6/game.js
+++ Activity6/game.js
@@ -1,41 +1,66 @@
-var button = document.getElementById("button");
-var game = document.getElementById("game");
+// Define variables
+var button = document.getElementById("myButton"); // ID is myButton, not button
+var scoreDisplay = document.getElementById("score");
 var score = 0;
 var level = 1;
 var timeout = 500;
 
+// Define function to move the button
 function moveButton() {
-  var x = Math.floor(Math.random() * (game.offsetWidth - button.offsetWidth));
-  var y = Math.floor(Math.random() * (game.offsetHeight - button.offsetHeight));
-  button.style.marginLeft = x + "px";
-  button.style.marginTop = y + "px";
+  var button = document.getElementById("myButton"); // ID is myButton, not button
+  var gameArea = document.getElementById("gameArea");
+  var gameAreaRect = gameArea.getBoundingClientRect();
+  var buttonRect = button.getBoundingClientRect();
+
+  var topBound = gameAreaRect.top;
+  var leftBound = gameAreaRect.left;
+  var bottomBound = gameAreaRect.bottom - buttonRect.height;
+  var rightBound = gameAreaRect.right - buttonRect.width;
+
+  var newTop = Math.floor(Math.random() * (bottomBound - topBound)) + topBound;
+  var newLeft = Math.floor(Math.random() * (rightBound - leftBound)) + leftBound;
+
+  button.style.top = newTop + "px";
+  button.style.left = newLeft + "px";
+
+  // Success message
+  // var successMsg = document.createElement("p");
+  // successMsg.textContent = "Good job! Keep going...";
+  // gameArea.appendChild(successMsg);
+  // setTimeout(() => {
+  //   successMsg.remove();
+  // }, 1000);
 }
 
-function catchButton() {
-  score++;
-  if (score === 3) {
-    alert("Level " + level + " completed! Congratulations!");
-    level++;
-    score = 0;
-    timeout -= 100;
-    if (level === 7) {
-      alert("You won the game! Congratulations, you have completed all the levels!");
-      return;
+// Add event listener to button
+button.addEventListener("click", function() {
+    // Increase score and update score display
+    score++;
+    scoreDisplay.innerHTML = "Score: " + score;
+
+    // Check if player has won the level
+    if (score % 3 === 0) {
+        // Increase level and decrease timeout
+        level++;
+        timeout -= 100;
+
+        // Display level message
+        alert("Congratulations! You have advanced to level " + level + ".");
+
+        // Reset score
+        score = 0;
+
+        // Set timeout for next level
+        if (level === 6) {
+            timeout = 0;
+        }
+        setTimeout(function() {
+            setInterval(moveButton, timeout);
+        }, 1000);
     }
-    setTimeout(function() {
-      alert("Level " + level + " starting!");
-      playGame();
-    }, 1000);
-  } else {
-    alert("You got the button! Now you need to catch " + score + " times");
-  }
-}
 
-function playGame() {
-  moveButton();
-  setTimeout(function() {
-    playGame();
-  }, timeout);
-}
-
-playGame();
+    // Check if player has won the game
+    if (level > 6) {
+        alert("Congratulations! You have completed the game.");
+    }
+});
